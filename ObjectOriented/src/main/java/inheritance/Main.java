90e04f0e846dcc89b8fbb25586c409c049529048
kai-page <kai.watanabe.kd@gmail.com>
2023-06-12 17:46:56 +0900

    ソースコード編集

--- ObjectOriented/src/main/java/inheritance/Main.java
+++ ObjectOriented/src/main/java/inheritance/Main.java
@@ -1,12 +1,22 @@
+/**
+ * Main.java
+ * 継承について
+ * @author KAI
+ */
+
 package inheritance;
 
 public class Main {
 	public static void main(String[] args) {
 		
+		// Personクラスを実装
 		Person person = new Person("AAA", 17);
+		// Personクラスのdisplayメソッドを呼び出す
 		person.display();
 		
+		// Studentクラスを実装
 		Student stu1 = new Student("BBB", 16, "XXX");
+		// Studentクラスのdisplayメソッドを呼び出す
 		stu1.display();
 		
 	}
--- ObjectOriented/src/main/java/inheritance/Person.java
+++ ObjectOriented/src/main/java/inheritance/Person.java
@@ -1,15 +1,23 @@
+/**
+ * Person.java
+ * 継承について
+ * @author KAI
+ */
+
 package inheritance;
 
 public class Person {
 	
-	private String name;
-	private int age;
+	private String name; // 名前
+	private int age; // 年齢
 	
+	// コンストラクタでメンバ変数の初期化を行う
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
 	
+	// 名前と年齢を表示するメソッド
 	public void display() {
 		System.out.println("名前: " + name);
 		System.out.println("年齢: " + age);
