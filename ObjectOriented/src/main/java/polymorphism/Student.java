a180d7a31804075e5c828f350bdd6f594fa24282
kai-page <kai.watanabe.kd@gmail.com>
2023-06-12 20:07:46 +0900

    ソースファイル追加

---
+++ ObjectOriented/src/main/java/polymorphism/Student.java
@@ -0,0 +1,28 @@
+/**
+ * Student.java
+ * 抽象クラスとポリモフィズムについて
+ * @author KAI
+ */
+
+package polymorphism;
+
+public class Student {
+	
+	private String name;
+	private Club club;
+	
+	public Student(String name, Club club) {
+		this.name = name;
+		this.club = club;
+	}
+	
+	public void display() {
+		System.out.println("名前: " + name);
+		club.display();
+	}
+	
+	public void practice() {
+		club.practice();
+	}
+	
+}
