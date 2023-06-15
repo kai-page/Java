7ddbd5d32d6e9cfa2e39ef6fce9f1d0ed7c6617f
kai-page <kai.watanabe.kd@gmail.com>
2023-06-15 18:36:25 +0900

    Basic追加

---
+++ Basic/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * HelloWorld.java
+ * Hello Worldを表示する
+ * @author KAI
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		// 画面表示
+		System.out.println("Hello World!");
+		
+	}
+}
