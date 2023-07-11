0c9e394621af35384acfa4d46421b858a9785c90
kai-page <kai.watanabe.kd@gmail.com>
2023-07-11 19:27:41 +0900

    MemoList追加

---
+++ MemoList/src/main/java/com/example/memolist/MemoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.memolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoListApplication.class, args);
+	}
+
+}
